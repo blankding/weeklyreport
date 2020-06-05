f99a323f20af86f9b586c52217e47dd7fc353849
dpf <19046074@cnsuning.com>
2020-06-05 20:45:05 +0800

    工具类

---
+++ src/main/java/com/example/weeklyreport/util/StringUtils.java
@@ -0,0 +1,12 @@
+package com.example.weeklyreport.util;
+
+/**
+ * <br>
+ * 〈功能详细描述〉
+ *      字符串工具类
+ * @author dpf
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class StringUtils {
+}
